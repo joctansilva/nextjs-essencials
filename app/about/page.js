398ac29677821a55749b05d72fa76047eded1430
Joctan Silva <joctan.silva@trinusco.com.br>
2024-04-10 23:25:16 -0300

    change the app to spa adding link component and config prettier

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,12 @@
+import Link from "next/link";
+
 export default function AboutPage() {
-  return 
-  <main>
-    <h1>About Us</h1>
-  </main>
+  return (
+    <main>
+      <h1>About Us</h1>{" "}
+      <h1>
+        <Link href="/">Return</Link>
+      </h1>
+    </main>
+  );
 }
