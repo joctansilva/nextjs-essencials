f704eef2b3c799a246884117c7821820d6cc95d8
Joctan Silva <joctannsilva@gmail.com>
2024-04-11 14:18:10 -0300

    separating header im some components, and ajust styles

--- components/Header/MainHeader.js
+++ components/Header/MainHeader.js
@@ -2,24 +2,28 @@ import Link from "next/link";
 import Image from "next/image";
 import logoImg from "../../assets/logo.png";
 import classes from "./MainHeader.module.css";
+import MainHeaderBackground from "./Background/MainHeaderBackground";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="A plate witch food on it" priority />
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>      
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate witch food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <MainHeaderBackground />
+    </>
   );
 }
