8a39fac2022ea3388f160f078cb3b362509eb532
Joctan Silva <joctannsilva@gmail.com>
2024-04-14 15:15:54 -0300

    create not found page

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return <main className="not-found">
+    <h1>ERROR 404</h1>
+    <p>Page not found</p>
+    <p>Unfortunately, we could no find the requersted page or resource.</p>
+  </main>
+}
