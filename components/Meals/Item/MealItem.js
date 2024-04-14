69b631008e48831dd8d5f55b2f028a0e71501b21
Joctan Silva <joctannsilva@gmail.com>
2024-04-14 00:13:31 -0300

    feat: config and create db in sql lite

--- components/Meals/Item/MealItem.js
+++ components/Meals/Item/MealItem.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import classes from "./MealItem.module.css";
 
-export default function MealItem({ title, id, image, summary, creator }) {
+export default function MealItem({ title, slug, image, summary, creator }) {
   return (
     <article className={classes.meal}>
       <header>
@@ -17,7 +17,7 @@ export default function MealItem({ title, id, image, summary, creator }) {
       <div className={classes.content}>
         <p className={classes.summary}>{summary}</p>
         <div className={classes.actions}>
-          <Link href={`/meals/${id}`}>View Details</Link>
+          <Link href={`/meals/${slug}`}>View Details</Link>
         </div>
       </div>
     </article>
